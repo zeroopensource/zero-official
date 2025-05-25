120a6f5b51378ef631e7a99e89fd0b063c23008a
Jigz <jigzpalillo@gmail.com>
2025-05-26 00:37:20 +0800

    docs(links): add descriptions to all social and support links
    Add meaningful descriptions to each link in the links.ts file to provide
    clear context about the purpose and content of the URLs. This improves
    usability and accessibility by helping users understand what each link
    offers, such as community engagement, project contributions, and support
    options.

--- packages/zero-official/src/links.ts
+++ packages/zero-official/src/links.ts
@@ -10,97 +10,103 @@ export const links: Link[] = [
   {
     name: 'ZERO_WEBSITE',
     url: 'https://zeroopensource.org/',
-    description: '',
+    description:
+      'Official website of Zero Open Source - Explore our projects and mission',
     icon: 'https://zeroopensource.org/favicon.ico',
   },
   {
     name: 'ZERO_DISCORD',
     url: 'https://discord.gg/2a5HcmxvgC',
-    description: '',
+    description:
+      'Join our community Discord server for discussions and support',
     icon: '',
   },
   {
     name: 'ZERO_GITHUB',
     url: 'https://github.com/zeroopensource',
-    description: '',
+    description:
+      'Browse our open source repositories and contribute to our projects',
     icon: '',
   },
   {
     name: 'ZERO_XTWITTER',
     url: 'https://x.com/ZeroOpenSource',
-    description: '',
+    description:
+      'Follow us on X (Twitter) for the latest updates and announcements',
     icon: '',
   },
   {
     name: 'ZERO_FACEBOOK',
     url: 'https://www.facebook.com/ZeroOpenSource',
-    description: '',
+    description: 'Connect with us on Facebook for community updates and events',
     icon: '',
   },
   {
     name: 'ZERO_REDDIT',
     url: 'https://www.reddit.com/r/ZeroOpenSource',
-    description: '',
+    description:
+      'Join our Reddit community for discussions and project updates',
     icon: '',
   },
   {
     name: 'ZERO_LINKEDIN',
     url: 'https://www.linkedin.com/company/zeroopensource',
-    description: '',
+    description:
+      'Follow our professional network for industry insights and opportunities',
     icon: '',
   },
   {
     name: 'ZERO_BLUESKY',
     url: 'https://bsky.app/profile/zeroopensource.bsky.social',
-    description: '',
+    description: 'Connect with us on Bluesky for decentralized social updates',
     icon: '',
   },
   {
     name: 'ZERO_MASTODON',
     url: 'https://mastodon.social/@ZeroOpenSource',
-    description: '',
+    description: 'Follow us on Mastodon for federated social updates',
     icon: '',
   },
   {
     name: 'ZERO_MEDIUM',
     url: 'https://medium.com/@zeroopensource',
-    description: '',
+    description: 'Read our in-depth articles and technical blog posts',
     icon: '',
   },
   {
     name: 'ZERO_DEVTO',
     url: 'https://dev.to/zeroopensource',
-    description: '',
+    description: 'Follow our developer-focused content and tutorials',
     icon: '',
   },
   {
     name: 'ZERO_DAILYDEV',
     url: 'https://app.daily.dev/zeroopensource',
-    description: '',
+    description: 'Check out our curated content for developers',
     icon: '',
   },
   {
     name: 'ZERO_HASHNODE',
     url: 'https://zeroopensource.hashnode.dev/',
-    description: '',
+    description: 'Read our technical blog posts and community articles',
     icon: '',
   },
   {
     name: 'ZERO_PATREON',
     url: 'https://www.patreon.com/c/ZeroOpenSource',
-    description: '',
+    description: 'Support our open source work through Patreon',
     icon: '',
   },
   {
     name: 'ZERO_BMC',
     url: 'https://buymeacoffee.com/zeroopensource',
-    description: '',
+    description: 'Show your support by buying us a coffee',
     icon: '',
   },
   // {
   //   name: 'ZERO_KOFI',
   //   url: 'https://ko-fi.com/zeroopensource',
-  //   description: '',
+  //   description: 'Support our projects through Ko-fi monthly or one-time donations',
   //   icon: '',
   //   disabled: true, // Unable to connect to stripe
   // },
